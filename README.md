812f49734ef0c98dac72d864a52df7f3cb9f4382
Isaac Vargas <ivcafe413@gmail.com>
2020-10-14 10:55:27 -0500

    Initial Runner/engine code

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # Astracraft (real name pending)
 
 Submission for itch.io Beginner's Circle Game Jam #3
+https://vagranttechnology.itch.io/astracraft
 
+## Getting the code
+https://github.com/ivcafe413/Astracraft.git
+
+Keeping the source code open for possible future tutorial/post-mortem
+
+## Debugging the game
+Run the entrypoint.py script to debug the game. A launch configuration for debugging is available in .vscode/launch.json (VS Code)
+
+## Building the game
+pip install pyinstaller
+pyinstaller entrypoint.py --name astracraft
+
+## Pusing the build
+Initial build channel is vagranttechnology/astracraft:windows-dev
+butler push dist/astracraft vagranttechnology/astracraft:windows-dev
---
+++ game/Runner.py
@@ -0,0 +1,99 @@
+# System level imports
+import sys
+import logging
+from collections import defaultdict
+
+# Third-party imports
+import pygame
+
+# Project package imports
+from game.GameState import GameState
+from game.Renderer import Renderer
+
+# Constants
+TARGET_FPS = 60
+LOOP_MS_PF = (1 / TARGET_FPS) * 1000
+
+# Class definition
+class Runner:
+    # Constructor function, creates new instance of class
+    def __init__(self): # 0 additional arguments, self is self-reference
+        pygame.init()
+        
+        # Game state object, tracks state of game and all objects in game
+        self.gameState = GameState() # Example of tightly coupled code.
+        # TODO: Refactor coupled code, separate dependencies
+
+        # Renderer, handles rendering functions on behalf of the game
+        rendererOptions = type('', (), {})()
+        rendererOptions.game = self.gameState
+        rendererOptions.width = 800
+        rendererOptions.height = 600
+        self.gameRenderer = Renderer(rendererOptions)
+        # TODO: Separate dependencies
+
+        # Internal variables to handle frame ticks/FPS
+        self.clock = pygame.time.Clock()
+        self.currentTime = 0
+
+        # Event Handler dictionaries
+        self.keydownHandlers = defaultdict(list)
+        self.keyupHandlers = defaultdict(list)
+
+    # Event Handler function
+    def handle_events(self):
+        # pygame.event.get() clears the event queue, prevents crashing/freezing
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit(0)
+            elif event.type == pygame.KEYDOWN:
+                for handler in self.keydownHandlers[event.key]:
+                    handler(event.type)
+            elif event.type == pygame.KEYUP:
+                for handler in self.keyupHandlers[event.key]:
+                    handler(event.type)
+
+    def start(self):
+        # Initial Key Bindings
+        # Sets actions for both key up and key down
+        # Could also handle other events (mouse events, etc.)
+        self.keydownHandlers[pygame.K_LEFT].append(self.gameState.action_left)
+        self.keydownHandlers[pygame.K_RIGHT].append(self.gameState.action_right)
+        self.keydownHandlers[pygame.K_UP].append(self.gameState.action_up)
+        self.keydownHandlers[pygame.K_DOWN].append(self.gameState.action_down)
+
+        self.keyupHandlers[pygame.K_LEFT].append(self.gameState.action_left)
+        self.keyupHandlers[pygame.K_RIGHT].append(self.gameState.action_right)
+        self.keyupHandlers[pygame.K_UP].append(self.gameState.action_up)
+        self.keyupHandlers[pygame.K_DOWN].append(self.gameState.action_down)
+
+        # Game Loop Start
+        # TODO: Implement some kind of "done/not done", not infinite loop
+        while True:
+            # Increment time tracker with ms passed since last call
+            self.currentTime += self.clock.get_time()
+            # Process current event queue
+            self.handle_events()
+
+            # While enough time has passed to process at least one frame
+            while self.currentTime >= LOOP_MS_PF:
+                # Update the game state one tick for each frame time block
+                self.gameState.update() # Passing in update MS diff?
+                self.currentTime -= LOOP_MS_PF
+
+            # Once game state is caught up, render the game
+            self.gameRenderer.render() # TODO: Passing in leftover ticks for delta rendering?
+
+            # Tracking FPS in window caption, should be for debug only
+            fps = self.clock.get_fps()
+            pygame.display.set_caption("FPS: {0:2f}".format(fps))
+
+            # Tell the system to wait until enough time has passed to hit the next frame
+            self.clock.tick(TARGET_FPS)
+
+def main():
+    # Sets logging to display on information logs and above
+    logging.basicConfig(level=logging.INFO)
+    # Start the game
+    Runner().start()
