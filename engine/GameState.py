3968e9e4083e5a76963262b85e70c63150ecd7ac
Deegan Townsend <DeeganTownsend@gmail.com>
2020-12-07 11:06:53 -0600

    resources

--- engine/GameState.py
+++ engine/GameState.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 from pyqtree import Index
 
@@ -17,6 +18,10 @@ class GameState:
         # Now tracking Game Win/Loss state variables
         self.timeElapsed = 0 # Keep track of total time (in frames) in GameState
         self.score = 0 # Initialize score to 0
+
+        # keeping track of spaces
+        self.emptySpaces = [*range(1, 97)]
+        self.filledSpaces = []
 
         # Method to create anonymous object
         options = type('', (), {})()
@@ -58,6 +63,27 @@ class GameState:
 
     def action_down(self, type):
         self.player.toggle_movement("down")
+    
+    def FindPos(self):
+        for x in self.filledSpaces:
+            if x in self.emptySpaces:
+                self.emptySpaces.remove(x)
+        PosVar = random.choice(self.emptySpaces)
+        self.filledSpaces.append(PosVar)
+        if PosVar == 1:
+            PosX=0
+            PosY=0
+        if PosVar == 17 or 33:
+            PosX=0
+            PosY=0
+        if PosVar == 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9 or 10 or 11 or 12 or 13 or 14 or 15 or 16:
+            PosVar=PosVar-1
+            PosX=50*PosVar
+            PosY=0
+        if PosVar == 18 or 19 or 20 or 21 or 22 or 23 or 24 or 25 or 26 or 27 or 28 or 29 or 30 or 31 or 32:
+            PosVar=PosVar-17
+            PosX=50*PosVar
+            PosY=50
 
     def update(self):
         collisionIndex = Index((0, 0, self.width, self.height)) # Create the empty collision index (Quad Tree)
@@ -75,11 +101,19 @@ class GameState:
                 CollisionHandler(collisions)
         
         if self.timeElapsed % 180 == 0:
+            for x in self.filledSpaces:
+                if x in self.emptySpaces:
+                    self.emptySpaces.remove(x)
+            StnVar = random.choice(self.emptySpaces)
+            self.filledSpaces.append(StnVar)
+            StnVar = StnVar/2
+            StnX = StnVar*50
+            StnY = StnVar*50
             hitzoneOptions = type('', (), {})()
-            hitzoneOptions.x = 400
+            hitzoneOptions.x = 0
             hitzoneOptions.y = 0
-            hitzoneOptions.w = 20
-            hitzoneOptions.h = 20
+            hitzoneOptions.w = 50
+            hitzoneOptions.h = 50
             hitzoneOptions.color = (255, 0, 0)
 
             self.hitzone = Hitzone(hitzoneOptions)
