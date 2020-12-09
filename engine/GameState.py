e466d51a5258598b128a07a0ecec709c17cc4e72
Isaac <isaac@Isaacs-MacBook-Pro.local>
2020-12-08 19:47:17 -0600

    Successfully validated new Environment, working on changing GameState win/loss conditions to match

--- engine/GameState.py
+++ engine/GameState.py
@@ -38,15 +38,15 @@ class GameState:
         self.gameObjects.add(self.player)
 
         # First Hitzone
-        # hitzoneOptions = type('', (), {})()
-        # hitzoneOptions.x = 400
-        # hitzoneOptions.y = 0
-        # hitzoneOptions.w = 20
-        # hitzoneOptions.h = 20
-        # hitzoneOptions.color = (255, 0, 0)
+        hitzoneOptions = type('', (), {})()
+        hitzoneOptions.x = 400
+        hitzoneOptions.y = 0
+        hitzoneOptions.w = 20
+        hitzoneOptions.h = 20
+        hitzoneOptions.color = (255, 0, 0)
 
-        # self.hitzone = Hitzone(hitzoneOptions)
-        # self.gameObjects.add(self.hitzone)
+        self.hitzone = Hitzone(hitzoneOptions)
+        self.gameObjects.add(self.hitzone)
 
         self.collidableObjects = list(filter(lambda o: o.collidable == True, self.gameObjects))
 
@@ -64,29 +64,31 @@ class GameState:
     def action_down(self, type):
         self.player.toggle_movement("down")
     
-    def FindPos(self):
-        for x in self.filledSpaces:
-            if x in self.emptySpaces:
-                self.emptySpaces.remove(x)
-        PosVar = random.choice(self.emptySpaces)
-        self.filledSpaces.append(PosVar)
-        if PosVar == 1:
-            PosX=0
-            PosY=0
-        if PosVar == 17 or 33:
-            PosX=0
-            PosY=0
-        if PosVar == 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9 or 10 or 11 or 12 or 13 or 14 or 15 or 16:
-            PosVar=PosVar-1
-            PosX=50*PosVar
-            PosY=0
-        if PosVar == 18 or 19 or 20 or 21 or 22 or 23 or 24 or 25 or 26 or 27 or 28 or 29 or 30 or 31 or 32:
-            PosVar=PosVar-17
-            PosX=50*PosVar
-            PosY=50
+    # def FindPos(self):
+    #     for x in self.filledSpaces:
+    #         if x in self.emptySpaces:
+    #             self.emptySpaces.remove(x)
+    #     PosVar = random.choice(self.emptySpaces)
+    #     self.filledSpaces.append(PosVar)
+    #     if PosVar == 1:
+    #         PosX=0
+    #         PosY=0
+    #     if PosVar == 17 or 33:
+    #         PosX=0
+    #         PosY=0
+    #     if PosVar == 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9 or 10 or 11 or 12 or 13 or 14 or 15 or 16:
+    #         PosVar=PosVar-1
+    #         PosX=50*PosVar
+    #         PosY=0
+    #     if PosVar == 18 or 19 or 20 or 21 or 22 or 23 or 24 or 25 or 26 or 27 or 28 or 29 or 30 or 31 or 32:
+    #         PosVar=PosVar-17
+    #         PosX=50*PosVar
+    #         PosY=50
 
     def update(self):
         collisionIndex = Index((0, 0, self.width, self.height)) # Create the empty collision index (Quad Tree)
+        # TODO: if(self.timeElapsed >= 1800) # 30s * 60fps = 1800f
+        # TODO: Game Over
         self.timeElapsed += 1
         for o in self.gameObjects:
             o.update()
@@ -100,21 +102,21 @@ class GameState:
                 # logging.info("Collision!")
                 CollisionHandler(collisions)
         
-        if self.timeElapsed % 180 == 0:
-            for x in self.filledSpaces:
-                if x in self.emptySpaces:
-                    self.emptySpaces.remove(x)
-            StnVar = random.choice(self.emptySpaces)
-            self.filledSpaces.append(StnVar)
-            StnVar = StnVar/2
-            StnX = StnVar*50
-            StnY = StnVar*50
-            hitzoneOptions = type('', (), {})()
-            hitzoneOptions.x = 0
-            hitzoneOptions.y = 0
-            hitzoneOptions.w = 50
-            hitzoneOptions.h = 50
-            hitzoneOptions.color = (255, 0, 0)
+        # if self.timeElapsed % 180 == 0:
+        #     for x in self.filledSpaces:
+        #         if x in self.emptySpaces:
+        #             self.emptySpaces.remove(x)
+        #     StnVar = random.choice(self.emptySpaces)
+        #     self.filledSpaces.append(StnVar)
+        #     StnVar = StnVar/2
+        #     StnX = StnVar*50
+        #     StnY = StnVar*50
+        #     hitzoneOptions = type('', (), {})()
+        #     hitzoneOptions.x = 0
+        #     hitzoneOptions.y = 0
+        #     hitzoneOptions.w = 50
+        #     hitzoneOptions.h = 50
+        #     hitzoneOptions.color = (255, 0, 0)
 
-            self.hitzone = Hitzone(hitzoneOptions)
-            self.gameObjects.add(self.hitzone)
+        #     self.hitzone = Hitzone(hitzoneOptions)
+        #     self.gameObjects.add(self.hitzone)
